9a13d41398b7873afa8e463d581cdc9e8eaed0a5
tkcannon <taylorkcannon@gmail.com>
2022-01-25 10:43:04 -0700

    added sessions

--- server.js
+++ server.js
@@ -3,14 +3,27 @@ const routes = require('./controllers/');
 const sequelize = require('./config/connection');
 const path = require('path');
 const exphbs = require('express-handlebars');
+const session = require('express-session');
 
 const app = express();
 const hbs = exphbs.create({});
 const PORT = process.env.PORT || 3001;
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+    secret: process.env.SESS_SECRET,
+    cookies: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+}
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session(sess));
 app.use(routes);
 
 app.engine('handlebars', hbs.engine);
